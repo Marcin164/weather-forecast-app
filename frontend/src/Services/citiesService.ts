fc56e8f8e59159e329c5306ccc56cdf04ca9ae95
Marcin <1999nowakowskimarcin@gmail.com>
2022-08-11 22:49:15 +0200

    change dotenv

--- frontend/src/Services/citiesService.ts
+++ frontend/src/Services/citiesService.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const fetchCities = async () => {
     try {
-        const cities = await axios.get(`${process.env.API_URL}/getCities`)
+        const cities = await axios.get(`http://localhost:3001/getCities`)
         if(!cities) throw Error("Cannot fetch cities")
         return cities
     } catch (error) {
--- frontend/src/Services/weatherService.ts
+++ frontend/src/Services/weatherService.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const fetchWeather = async () => {
     try {
-        const weather = await axios.get(`${process.env.API_URL}/getWeather`)
+        const weather = await axios.get(`http://localhost:3001/getWeather`)
         if(!weather) throw Error("Cannot fetch weather")
         return weather
     } catch (error) {
